cb6c68a802348853a3085fdc2eee4fe176eea74c
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2019-11-13 01:21:28 -0500

    Integrated Firebase to config/envirnment.js

--- config/environment.js
+++ config/environment.js
@@ -1,22 +1,34 @@
 'use strict';
 
 module.exports = function(environment) {
+  
   let ENV = {
     modulePrefix: 'news-buddy',
     environment,
     rootURL: '/',
     locationType: 'auto',
+    
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
+
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
         Date: false
       }
     },
-
+    firebase: {
+      apiKey: "AIzaSyA1Qnjifczr0m5kZM8bSlbjsTJe97jJinU",
+      authDomain: "news-buddy-87253.firebaseapp.com",
+      databaseURL: "https://news-buddy-87253.firebaseio.com",
+      projectId: "news-buddy-87253",
+      storageBucket: "news-buddy-87253.appspot.com",
+      messagingSenderId: "1011340472134",
+      appId: "1:1011340472134:web:3896f9a2ee52fa474d3f47",
+      measurementId: "G-QWBR8SKLR9" 
+    },
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
