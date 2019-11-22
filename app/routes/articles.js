4fbbf151ef79fa0b3bf28cec523fe90ff9c1502b
mslazak22 <m_slazak@yahoo.com>
2019-11-22 16:38:35 -0500

    bugs fixed

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -1,9 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model() {
+  
+    model(params) {
+      
         return this.store.query('search', {
-          q: 'election'
+            q: params.q
          
         });
       }
